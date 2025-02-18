7ac363bcab34b20d5a8629f556f00d574149e62e
Itz-preya <159290385+Itz-preya@users.noreply.github.com>
2025-02-18 21:47:01 +0530

    admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import participants
-from .models import organizer
+from .models import organizer,gdg
 # Register your models here.
 admin.site.register(participants)
 admin.site.register(organizer)
+admin.site.register(gdg)
