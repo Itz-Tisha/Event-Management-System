79cdb36f5ee572ca38f94b870040b5be5fd8209e
Itz-preya <159290385+Itz-preya@users.noreply.github.com>
2025-02-18 21:46:24 +0530

    models.py

--- models.py
+++ models.py
@@ -14,3 +14,14 @@ class organizer(models.Model):
     name=models.CharField(max_length=10)
     email=models.CharField(max_length=20) 
     password=models.CharField(max_length=8)
+     
+class gdg(models.Model):
+    event_name=models.CharField(max_length=20)
+    location=models.CharField(max_length=30)
+    date=models.DateField()
+    starttime=models.TimeField()
+    endtime=models.TimeField()
+    desc=models.CharField(max_length=50)
+    purpose_of_even=models.CharField(max_length=50)
+    event_img=models.ImageField()
+    conact=models.IntegerField()
