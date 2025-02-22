eb9e194b9c4d89a27ca18c55395ca835676a5c0d
Itz-Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-02-22 23:39:53 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('create_club/', views.create_club, name='create_club'),
     path('create_event/', views.create_event, name='create_event'),
     path('register/<int:event_id>/', views.register_for_event, name='register_for_event'),
-   
+   path('events/<str:clubname>/', views.club_events, name='club_events'),
      
 ]
