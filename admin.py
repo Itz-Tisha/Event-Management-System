0c89978a4f944a076ae5d6d5b0de370ad98d48c2
Itz-Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-03-22 18:19:24 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import UserType,club,event,event_reg
+from . models import UserType,club,event,event_reg,feedback
 admin.site.register(UserType)
 admin.site.register(club)
 admin.site.register(event)
 admin.site.register(event_reg)
+admin.site.register(feedback)
