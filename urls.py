ea904cb775b455910c855041a613731b1a791b7e
Itz-Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-03-22 18:20:49 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('create_club/', views.create_club, name='create_club'),
     path('create_event/', views.create_event, name='create_event'),
     path('register/<int:event_id>/', views.register_for_event, name='register_for_event'),
-   path('events/<str:clubname>/', views.club_events, name='club_events'),
-    
+    path('events/<str:clubname>/', views.club_events, name='club_events'),
+    path('edit_details/', views.edit_details, name='edit_details'),
+     path('give_Feedback/<int:event_id>',views.give_Feedback,name='give_Feedback'),
+     path('view_Feedback/<int:event_id>',views.view_Feedback,name='view_Feedback'),
 ]
